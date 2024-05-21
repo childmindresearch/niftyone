26d85e3f31fd7d747bc21999bfc0d7c63d1a5759
Jason Kai <jason.kai@childmind.org>
2024-05-21 13:26:23 -0400

    Rename pipelines -> analysis_levels
    - Rename module to better reflect its use
    - Moved `launch` to be part of `analysis_levels` module.
    - Updated `__init__.py`'s as necessary.

--- src/niftyone/__init__.py
+++ src/niftyone/__init__.py
@@ -1,4 +1,3 @@
 """Large-scale neuroimaging visualization using FiftyOne."""
 
 from ._version import __version__, __version_tuple__
-from .launch import launch
--- src/niftyone/pipelines/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Initialize module for generating pipelines."""
-
-from .group import group_pipeline as group
-from .participant import (
-    participant_pipeline as participant,
-)
