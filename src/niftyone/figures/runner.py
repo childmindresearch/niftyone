ddd6dbc37e73e192f7fce202b2b8447a24654ef3
Jason Kai <jason.kai@childmind.org>
2024-05-22 13:39:45 -0400

    Remove unneeded figure generation code
    - Left legacy code in while developing generators. This commit just
      removes all of the legacy code.
    - Also renamed `generators` -> `figure_generators` in case we want to
      pull out the Runner from figure module to be more generic.

--- src/niftyone/figures/runner.py
+++ src/niftyone/figures/runner.py
@@ -10,9 +10,9 @@ from niftyone.figures.generator import ViewGenerator
 class Runner:
     """Runner class for generating figures."""
 
-    def __init__(self, generators: list[ViewGenerator]) -> None:
-        self.generators = generators
+    def __init__(self, figure_generators: list[ViewGenerator]) -> None:
+        self.figure_generators = figure_generators
 
     def run(self, table: BIDSTable, out_dir: Path, overwrite: bool = False) -> None:
-        for generator in self.generators:
-            generator(table=table, out_dir=out_dir, overwrite=overwrite)
+        for figure_generator in self.figure_generators:
+            figure_generator(table=table, out_dir=out_dir, overwrite=overwrite)
