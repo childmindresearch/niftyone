aefc0b507d53177ce6c5d209b616077583405e38
Jason Kai <jason.kai@childmind.org>
2024-05-30 14:24:05 -0400

    Include subsections to niftyone docs

--- src/niftyone/__init__.py
+++ src/niftyone/__init__.py
@@ -1,5 +1,9 @@
 """
 .. include:: ../../README.md
+.. include:: ../../docs/usage.md
+.. include:: ../../CONTRIBUTING.md
+.. include:: ../../docs/contributing/setup.md
+.. include:: ../../docs/faq.md
 """
 
 from ._version import __version__, __version_tuple__
