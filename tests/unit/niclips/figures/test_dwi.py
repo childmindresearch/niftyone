c0652137b66e27fe60c8bec8c53f33ea71b4db64
Jason Kai <jason.kai@childmind.org>
2024-07-24 12:14:54 -0400

    Add fig generation for dwi grad in qspace
    - Q-space generation still uses the dwi nifti (to avoid filtering for
      additional extensions and having a major change to view_fn
    functionality). Instead, we grab the bval and bvecs by replacing the
    extension appropriately and asserting that its existence (which it
    should if following BIDS conventions)
    - Added input_type checking for generator to pass along either image or
      path as needed. Note this not change previous functionality, aside
    from passing the image path instead of always passing a nifti image
    object
    - Added some helper functions (may end up getting moved to a utility
      module later on) to help with equating and extracting bvals.
    - Also added unit tests for generation

---
+++ tests/unit/niclips/figures/test_dwi.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from niclips.figures import dwi as nodwi
+
+
+@pytest.fixture
+def dwi_fpath(tmp_path: Path) -> Path:
+    bvals = np.array([5, 1600, 1600, 1600, 1595, 1600, 1605, 1595, 1600, 1595, 1595])
+    bval_fpath = tmp_path / "test.bval"
+    np.savetxt(bval_fpath, bvals)
+
+    bvecs = np.array(
+        [
+            [
+                0.57735026,
+                -0.9999969,
+                -0.586878,
+                -0.8942548,
+                -0.72977316,
+                0.15373544,
+                -0.11708178,
+                0.40867075,
+                -0.21039648,
+                -0.09445284,
+                0.13537486,
+            ],
+            [
+                0.57735032,
+                0.0,
+                0.71746105,
+                -0.44684735,
+                -0.54764342,
+                0.21455349,
+                0.62163621,
+                0.81375086,
+                -0.62104523,
+                -0.22652406,
+                0.99029183,
+            ],
+            [
+                -0.57735038,
+                0.00250311,
+                -0.37526515,
+                0.02521372,
+                -0.40928939,
+                0.96453744,
+                0.77450669,
+                -0.41327715,
+                0.75500739,
+                -0.96941489,
+                -0.03155597,
+            ],
+        ]
+    )
+    bvec_fpath = tmp_path / "test.bvec"
+    np.savetxt(bvec_fpath, bvecs)
+
+    return tmp_path / "test.nii.gz"
+
+
+class TestQSpaceShells:
+    @pytest.mark.parametrize("thresh", [(5), (10), (30)])
+    def test_default(self, dwi_fpath: Path, thresh: int):
+        nodwi.visualize_shells(dwi=dwi_fpath, thresh=thresh)
+
+    def test_save(self, dwi_fpath: Path, tmp_path: Path):
+        out_fpath = tmp_path / "test_qspace.mp4"
+        nodwi.visualize_shells(dwi=dwi_fpath, out=out_fpath)
+
+        assert out_fpath.exists()
